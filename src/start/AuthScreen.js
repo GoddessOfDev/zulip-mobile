GoddessOfDev/zulip-mobile

commit 865914f0e3f00071257d544f1d164207a2907b66

accounts: Make `Auth.realm` a URL object, including in `state.accounts`.
Changing the `Auth` type implicitly changes the `Account` and
`Identity` types, too.

Done with an effort to minimize unnecessary follow-on changes in
this commit. Here, we don't propagate the realm in its URL form as
far toward the eventual consumers of the realm as we'll eventually
want to; we'll do that in upcoming commits.

Wherever two realm values are compared for equality with `===`, we
make sure they are both stringified first. Flow didn't warn about
these cases, so we caught them in test failures and by scanning
through a full-project search for the term 'realm'.

src/start/AuthScreen.js
@@ -226,7 +226,7 @@ class AuthScreen extends PureComponent<Props> {
     const { dispatch, realm } = this.props;
     const auth = webAuth.authFromCallbackUrl(event.url, otp, realm);
     if (auth) {
-      dispatch(loginSuccess(auth.realm, auth.email, auth.apiKey));
+      dispatch(loginSuccess(auth.realm.toString(), auth.email, auth.apiKey));
     }
   };
 
@@ -347,5 +347,5 @@ class AuthScreen extends PureComponent<Props> {
 }
 
 export default connect(state => ({
-  realm: getCurrentRealm(state),
+  realm: getCurrentRealm(state).toString(),
 }))(AuthScreen);

src/__tests__/lib/exampleData.js
@@ -123,7 +123,7 @@ export const makeCrossRealmBot = (args: { name?: string } = {}): CrossRealmBot =
     is_bot: true,
   });
 
-export const realm = 'https://zulip.example.org';
+export const realm = new URL('https://zulip.example.org');
 
 export const zulipVersion = new ZulipVersion('2.1.0-234-g7c3acf4');
 
@@ -133,7 +133,7 @@ export const makeAccount = (
   args: {
     user?: User,
     email?: string,
-    realm?: string,
+    realm?: URL,
     apiKey?: string,
     zulipFeatureLevel?: number | null,
     zulipVersion?: ZulipVersion | null,
@@ -423,7 +423,7 @@ export const action = deepFreeze({
   },
   login_success: {
     type: LOGIN_SUCCESS,
-    realm: selfAccount.realm,
+    realm: selfAccount.realm.toString(),
     email: selfAccount.email,
     apiKey: selfAccount.apiKey,
   },
@@ -475,7 +475,7 @@ export const action = deepFreeze({
       realm_send_welcome_emails: true,
       realm_show_digest_email: true,
       realm_signup_notifications_stream_id: 3,
-      realm_uri: selfAccount.realm,
+      realm_uri: selfAccount.realm.toString(),
       realm_video_chat_provider: 1,
       realm_waiting_period_threshold: 3,
       zulip_feature_level: 1,

src/account-info/AccountDetails.js
@@ -80,6 +80,6 @@ class AccountDetails extends PureComponent<Props> {
 }
 
 export default connect<SelectorProps, _, _>((state, props) => ({
-  realm: getCurrentRealm(state),
+  realm: getCurrentRealm(state).toString(),
   userStatusText: getUserStatusTextForUser(state, props.user.user_id),
 }))(AccountDetails);

src/account/AccountList.js
@@ -20,14 +20,14 @@ export default class AccountList extends PureComponent<Props> {
       <View>
         <FlatList
           data={accounts}
-          keyExtractor={item => `${item.email}${item.realm}`}
+          keyExtractor={item => `${item.email}${item.realm.toString()}`}
           ItemSeparatorComponent={() => <ViewPlaceholder height={8} />}
           renderItem={({ item, index }) => (
             <AccountItem
               index={index}
               showDoneIcon={index === 0 && item.isLoggedIn}
               email={item.email}
-              realm={item.realm}
+              realm={item.realm.toString()}
               onSelect={onAccountSelect}
               onRemove={onAccountRemove}
             />

src/account/AccountPickScreen.js
@@ -25,7 +25,7 @@ class AccountPickScreen extends PureComponent<Props> {
         dispatch(switchAccount(index));
       });
     } else {
-      dispatch(navigateToRealmScreen(realm));
+      dispatch(navigateToRealmScreen(realm.toString()));
     }
   };
 

src/account/__tests__/accountsReducer-test.js
@@ -17,8 +17,8 @@ import * as eg from '../../__tests__/lib/exampleData';
 describe('accountsReducer', () => {
   describe('REALM_ADD', () => {
     describe('on list of identities', () => {
-      const account1 = eg.makeAccount({ realm: 'https://realm.one.org', apiKey: '' });
-      const account2 = eg.makeAccount({ realm: 'https://realm.two.org', apiKey: '' });
+      const account1 = eg.makeAccount({ realm: new URL('https://realm.one.org'), apiKey: '' });
+      const account2 = eg.makeAccount({ realm: new URL('https://realm.two.org'), apiKey: '' });
       const prevState = deepFreeze([account1, account2]);
       const baseAction = deepFreeze({
         type: REALM_ADD,
@@ -27,8 +27,8 @@ describe('accountsReducer', () => {
       });
 
       test('if no account with this realm exists, prepend new one, with empty email/apiKey', () => {
-        const newRealm = 'https://new.realm.org';
-        const action = deepFreeze({ ...baseAction, realm: newRealm });
+        const newRealm = new URL('https://new.realm.org');
+        const action = deepFreeze({ ...baseAction, realm: newRealm.toString() });
         expect(accountsReducer(prevState, action)).toEqual([
           eg.makeAccount({ realm: newRealm, email: '', apiKey: '' }),
           account1,
@@ -37,7 +37,7 @@ describe('accountsReducer', () => {
       });
 
       test('if account with this realm exists, move to front of list', () => {
-        const action = deepFreeze({ ...baseAction, realm: account2.realm });
+        const action = deepFreeze({ ...baseAction, realm: account2.realm.toString() });
         expect(accountsReducer(prevState, action)).toEqual([account2, account1]);
       });
     });
@@ -46,7 +46,7 @@ describe('accountsReducer', () => {
       const existingAccountBase = eg.makeAccount({});
       const baseAction = deepFreeze({
         type: REALM_ADD,
-        realm: existingAccountBase.realm,
+        realm: existingAccountBase.realm.toString(),
         zulipFeatureLevel: eg.zulipFeatureLevel,
         zulipVersion: eg.zulipVersion,
       });
@@ -153,8 +153,8 @@ describe('accountsReducer', () => {
   });
 
   describe('LOGIN_SUCCESS', () => {
-    const account1 = eg.makeAccount({ email: '', realm: 'https://one.example.org' });
-    const account2 = eg.makeAccount({ realm: 'https://two.example.org' });
+    const account1 = eg.makeAccount({ email: '', realm: new URL('https://one.example.org') });
+    const account2 = eg.makeAccount({ realm: new URL('https://two.example.org') });
 
     const prevState = deepFreeze([account1, account2]);
 
@@ -168,7 +168,7 @@ describe('accountsReducer', () => {
         type: LOGIN_SUCCESS,
         apiKey: newAccount.apiKey,
         email: newAccount.email,
-        realm: newAccount.realm,
+        realm: newAccount.realm.toString(),
       });
 
       const expectedState = [{ ...newAccount, zulipVersion: account1.zulipVersion }, account2];
@@ -181,7 +181,7 @@ describe('accountsReducer', () => {
     test('on login, if account does not exist, add as first item', () => {
       const newAccount = eg.makeAccount({
         email: 'newaccount@example.com',
-        realm: 'https://new.realm.org',
+        realm: new URL('https://new.realm.org'),
         zulipVersion: null,
         zulipFeatureLevel: null,
       });
@@ -190,7 +190,7 @@ describe('accountsReducer', () => {
         type: LOGIN_SUCCESS,
         apiKey: newAccount.apiKey,
         email: newAccount.email,
-        realm: newAccount.realm,
+        realm: newAccount.realm.toString(),
       });
 
       const expectedState = [newAccount, account1, account2];
@@ -209,7 +209,7 @@ describe('accountsReducer', () => {
       const action = deepFreeze({
         type: LOGIN_SUCCESS,
         apiKey: newAccount.apiKey,
-        realm: newAccount.realm,
+        realm: newAccount.realm.toString(),
         email: newAccount.email,
       });
 

src/account/accountMisc.js
@@ -8,7 +8,8 @@ export const identityOfAuth: Auth => Identity = identitySlice;
 export const identityOfAccount: Account => Identity = identitySlice;
 
 /** A string corresponding uniquely to an identity, for use in `Map`s. */
-export const keyOfIdentity = ({ realm, email }: Identity): string => `${realm}\0${email}`;
+export const keyOfIdentity = ({ realm, email }: Identity): string =>
+  `${realm.toString()}\0${email}`;
 
 export const authOfAccount = (account: Account): Auth => {
   const { realm, email, apiKey } = account;

src/account/accountsReducer.js
@@ -16,7 +16,7 @@ import { NULL_ARRAY } from '../nullObjects';
 const initialState = NULL_ARRAY;
 
 const realmAdd = (state, action) => {
-  const accountIndex = state.findIndex(account => account.realm === action.realm);
+  const accountIndex = state.findIndex(account => account.realm.toString() === action.realm);
 
   if (accountIndex !== -1) {
     const newAccount = {
@@ -29,7 +29,7 @@ const realmAdd = (state, action) => {
 
   return [
     {
-      realm: action.realm,
+      realm: new URL(action.realm),
       apiKey: '',
       email: '',
       ackedPushToken: null,
@@ -60,16 +60,24 @@ const accountSwitch = (state, action) => {
 const findAccount = (state: AccountsState, identity: Identity): number => {
   const { realm, email } = identity;
   return state.findIndex(
-    account => account.realm === realm && (!account.email || account.email === email),
+    account =>
+      account.realm.toString() === realm.toString() && (!account.email || account.email === email),
   );
 };
 
 const loginSuccess = (state, action) => {
   const { realm, email, apiKey } = action;
-  const accountIndex = findAccount(state, { realm, email });
+  const accountIndex = findAccount(state, { realm: new URL(realm), email });
   if (accountIndex === -1) {
     return [
-      { realm, email, apiKey, ackedPushToken: null, zulipVersion: null, zulipFeatureLevel: null },
+      {
+        realm: new URL(realm),
+        email,
+        apiKey,
+        ackedPushToken: null,
+        zulipVersion: null,
+        zulipFeatureLevel: null,
+      },
       ...state,
     ];
   }

commit 16283f7f5698d812df5706dc255b5eeaa30cda5e

AuthScreen: Stop doubling a slash in Apple auth endpoint.
For #4146, but also to fix a recent regression from 865914f0e, like
we did in b3af772ed.

src/start/AuthScreen.js
@@ -260,8 +260,7 @@ class AuthScreen extends PureComponent<Props> {
       id_token: credential.identityToken,
     });
 
-    // TODO: Use a `URL` computation, for #4146.
-    openLink(`${this.props.realm.toString()}/complete/apple/?${params}`);
+    openLink(new URL(`/complete/apple/?${params}`, this.props.realm).toString());
 
     // Currently, the rest is handled with the `zulip://` redirect,
     // same as in the web flow.