GoddessOfDev/zulip-mobile

commit ed69e1ec030bb602c7c652275559f1abcd7463dd

UserAvatar [nfc]: Stop using `ImageBackground`.
This will allow us to use an `Icon` in place of a avatar URL in the
future. The implementation of `ImageBackground` is very simple, we
essentially inline it.

src/common/UserAvatar.js
@@ -1,6 +1,6 @@
 /* @flow strict-local */
 import React, { type Node as React$Node } from 'react';
-import { ImageBackground, View, PixelRatio } from 'react-native';
+import { Image, View, PixelRatio } from 'react-native';
 
 import { useSelector } from '../react-redux';
 import { getAuthHeaders } from '../api/transport';
@@ -46,21 +46,20 @@ function UserAvatar(props: Props) {
 
   return (
     <View>
-      <Touchable onPress={onPress} style={style}>
-        <ImageBackground
-          style={style}
-          source={{
-            uri: avatarUrl.get(PixelRatio.getPixelSizeForLayoutSize(size)).toString(),
-            ...(avatarUrl instanceof FallbackAvatarURL
-              ? { headers: getAuthHeaders(auth) }
-              : undefined),
-          }}
-          resizeMode="cover"
-          /* ImageBackground seems to ignore `style.borderRadius`. */
-          borderRadius={borderRadius}
-        >
+      <Touchable onPress={onPress}>
+        <View>
+          <Image
+            source={{
+              uri: avatarUrl.get(PixelRatio.getPixelSizeForLayoutSize(size)).toString(),
+              ...(avatarUrl instanceof FallbackAvatarURL
+                ? { headers: getAuthHeaders(auth) }
+                : undefined),
+            }}
+            style={style}
+            resizeMode="cover"
+          />
           {children}
-        </ImageBackground>
+        </View>
       </Touchable>
     </View>
   );

commit 29df234cfa5b5be2e0dba91c7519232a423ba46f

UserAvatar: Ignore color inversion on iOS.
ed69e1ec0 inadvertently removed this when we switched away from
ImageBackground, but it seems like something that we should include,
since avatars will look better non-inverted, and inverting arbitrary
images isn't likely to be very useful for contrast/accessibility
reasons.

Fixes: #4765

src/common/UserAvatar.js
@@ -56,7 +56,7 @@ function UserAvatar(props: Props) {
 
   return (
     <Touchable onPress={onPress}>
-      <View>
+      <View accessibilityIgnoresInvertColors>
         {!isMuted ? (
           <Image
             source={{